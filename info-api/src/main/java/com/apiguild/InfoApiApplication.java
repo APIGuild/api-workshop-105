2eb521b4aedcdff3b149297ecc0ebc4691338e73
Bing Wang <bingwang@thoughtworks.com>
2017-10-30 16:44:04 +0800

    add info

---
+++ info-api/src/main/java/com/apiguild/InfoApiApplication.java
@@ -0,0 +1,12 @@
+package com.apiguild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoApiApplication.class, args);
+	}
+}
